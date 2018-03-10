12766daa7ba1a5dafeacb4f124d81ae1cc5e8fc6
Brennan Heisler <learn@heislercreative.com>
2018-03-10 05:19:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,7 +80,9 @@ class Student
       LIMIT ?
     SQL
     
-    DB[:conn].execute(sql, x)
+    DB[:conn].execute(sql, x).collect do |row|
+      self.new_from_db(row)
+    end
   end
   
   def self.first_student_in_grade_10
