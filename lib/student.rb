f197e7b5cb87b015f3a4d078504d05f35b1ecc0a
Brennan Heisler <learn@heislercreative.com>
2018-03-10 05:19:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,9 @@ class Student
       WHERE grade = 9
     SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
   end
   
   def self.students_below_12th_grade
