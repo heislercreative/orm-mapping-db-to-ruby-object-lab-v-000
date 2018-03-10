7330f0b8c7cb8715d2e6981ef01995291a0f1bd3
Brennan Heisler <learn@heislercreative.com>
2018-03-10 05:12:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -93,4 +93,14 @@ class Student
     DB[:conn].execute(sql)
   end
   
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+    
+    DB[:conn].execute(sql, x)
+  end
+  
 end
